f5d9f288bf1499e3c0a70364ba6beb67b507000c
theodorous beatayuvens <theochandra27@gmail.com>
2021-03-26 20:45:04 +0700

    Initial commit

---
+++ app/src/main/java/com/android/and_movie_fetcher/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.and_movie_fetcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
