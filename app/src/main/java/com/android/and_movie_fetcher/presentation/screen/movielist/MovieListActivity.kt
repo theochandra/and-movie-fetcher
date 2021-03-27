167baf93aa8d8bebf2e7653398ee1dd42584179c
theodorous beatayuvens <theochandra27@gmail.com>
2021-03-27 11:57:06 +0700

    refactor main activity

---
+++ app/src/main/java/com/android/and_movie_fetcher/presentation/screen/movielist/MovieListActivity.kt
@@ -0,0 +1,12 @@
+package com.android.and_movie_fetcher.presentation.screen.movielist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.and_movie_fetcher.R
+
+class MovieListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_list)
+    }
+}
